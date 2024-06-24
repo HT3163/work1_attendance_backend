69209f5f07654091a357e7051b45d1d71b50cdbf
HT3163 <hamzagujjar3163@gmail.com>
2024-06-24 20:30:04 +0500

    adding hellow world get api

--- index.js
+++ index.js
@@ -16,5 +16,9 @@ app.use(express.json({ extended: true }));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
 
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
